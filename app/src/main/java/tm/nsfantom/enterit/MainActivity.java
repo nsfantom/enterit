769b1e408b51f1b241482390c671de31cb19b81f
Mykola Tatarenkov <tatarenkov.ms@gbksoft.com>
2018-11-12 14:50:44 +0200

    Initial commit

---
+++ app/src/main/java/tm/nsfantom/enterit/MainActivity.java
@@ -0,0 +1,13 @@
+package tm.nsfantom.enterit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
